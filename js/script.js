acc0b972e61a7fba8b807085f2969165e872df26
Fazilabi <fazilag@code.edu.az>
2019-04-14 15:52:15 +0400

    14.04.19-basic loops

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// var m=100;
+// var i=1;
+
+// while(i<=m){
+//     if(i%3==0&&i%4==0 ){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// var n=324;
+// var i=1;
+// while(i<=n){
+//     if(n%i==0){
+//     console.log(i);
+//     }i++;
+// }
